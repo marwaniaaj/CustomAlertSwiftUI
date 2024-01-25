95b36e7aee6dff9ab02df6c16cc27dc0eb6484dc
Marwa <marwa.niaaj@gmail.com>
2024-01-25 10:55:26 +0400

    Add `CustomAlertView`.

---
+++ CustomAlert/CustomAlertView.swift
@@ -0,0 +1,199 @@
+//
+//  CustomAlertView.swift
+//  CustomAlert
+//
+//  Created by Marwa Abou Niaaj on 25/01/2024.
+//
+
+import SwiftUI
+
+struct CustomAlertView<T: Hashable, M: View>: View {
+
+    @Namespace private var namespace
+
+    @Binding private var isPresented: Bool
+    @State private var titleKey: LocalizedStringKey
+    @State private var actionTextKey: LocalizedStringKey
+
+    private var data: T?
+    private var actionWithValue: ((T) -> ())?
+    private var messageWithValue: ((T) -> M)?
+
+    private var action: (() -> ())?
+    private var message: (() -> M)?
+
+    // Animation
+    @State private var isAnimating = false
+    private let animationDuration = 0.5
+
+    init(
+        _ titleKey: LocalizedStringKey,
+        _ isPresented: Binding<Bool>,
+        presenting data: T?,
+        actionText: String,
+        action: @escaping (T) -> (),
+        @ViewBuilder message: @escaping (T) -> M
+    ) {
+        _titleKey = State(wrappedValue: titleKey)
+        _actionTextKey = State(wrappedValue: LocalizedStringKey(actionText))
+        _isPresented = isPresented
+
+        self.data = data
+        self.action = nil
+        self.message = nil
+        self.actionWithValue = action
+        self.messageWithValue = message
+    }
+
+    var body: some View {
+        ZStack {
+            Color.gray
+                .ignoresSafeArea()
+                .opacity(isPresented ? 0.6 : 0)
+
+            if isAnimating {
+                VStack {
+                    VStack {
+                        /// Title
+                        Text(titleKey)
+                            .font(.title2).bold()
+                            .foregroundStyle(.tint)
+                            .padding(8)
+
+                        /// Message
+                        Group {
+                            if let data, let messageWithValue {
+                                messageWithValue(data)
+                            } else if let message {
+                                message()
+                            }
+                        }
+                        .multilineTextAlignment(.center)
+
+                        /// Buttons
+                        HStack {
+                            CancelButton
+                            DoneButton
+                        }
+                        .fixedSize(horizontal: false, vertical: true)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.background)
+                    .cornerRadius(35)
+                }
+                .padding()
+            }
+        }
+        .ignoresSafeArea()
+        .zIndex(.greatestFiniteMagnitude)
+        .onAppear {
+            show()
+        }
+    }
+
+    // MARK: Buttons
+    var CancelButton: some View {
+        Button {
+            dismiss()
+        } label: {
+            Text("Cancel")
+                .font(.headline)
+                .foregroundStyle(.tint)
+                .padding()
+                .lineLimit(1)
+                .frame(maxWidth: .infinity)
+                .background(Material.regular)
+                .background(.gray)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+        }
+    }
+
+    var DoneButton: some View {
+        Button {
+            dismiss()
+
+            if let data, let actionWithValue {
+                actionWithValue(data)
+            } else if let action {
+                action()
+            }
+        } label: {
+            Text(actionTextKey)
+                .font(.headline).bold()
+                .foregroundStyle(Color.white)
+                .padding()
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity)
+                .background(.tint)
+                .clipShape(RoundedRectangle(cornerRadius: 30.0))
+        }
+    }
+
+    func dismiss() {
+        withAnimation(.easeInOut(duration: animationDuration)) {
+            isAnimating = false
+            isPresented = false
+        }
+    }
+
+    func show() {
+        withAnimation(.easeInOut(duration: animationDuration)) {
+            isAnimating = true
+        }
+    }
+}
+
+// MARK: - Overload
+extension CustomAlertView where T == Never {
+
+    init(
+        _ titleKey: LocalizedStringKey,
+        _ isPresented: Binding<Bool>,
+        actionText: String,
+        action: @escaping () -> (),
+        @ViewBuilder message: @escaping () -> M
+    ) where T == Never {
+        _titleKey = State(wrappedValue: titleKey)
+        _actionTextKey = State(wrappedValue: LocalizedStringKey(actionText))
+        _isPresented = isPresented
+
+        self.data = nil
+        self.action = action
+        self.message = message
+        self.actionWithValue = nil
+        self.messageWithValue = nil
+    }
+}
+
+// MARK: - Preview
+struct CustomAlertPreview: View {
+    @State private var isPresented = false
+    @State private var test = "Some Value"
+
+    var body: some View {
+        ZStack {
+            Button("Show Alert") {
+                isPresented = true
+            }
+            if isPresented {
+                CustomAlertView(
+                    "Alert Title",
+                    $isPresented,
+                    presenting: test,
+                    actionText: "Yes, Done"
+                ) { value in
+                    // Action...
+                } message: { value in
+                    Text("Showing alert for \(value)… And adding a long text for preview.")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CustomAlertPreview()
+}
