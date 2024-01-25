1015769511e395aa1f91c8ada45b5d27705bc9fd
Marwa <marwa.niaaj@gmail.com>
2024-01-25 10:26:34 +0400

    Implement starter code.

--- CustomAlert/ContentView.swift
+++ CustomAlert/ContentView.swift
@@ -7,15 +7,69 @@
 
 import SwiftUI
 
+struct Book: Identifiable, Hashable {
+    var id: UUID { UUID() }
+    var title: String
+    var author: String
+}
+
 struct ContentView: View {
+
+    @State private var books = [
+        Book(title: "Atomic Habits", author: "James Clear"),
+        Book(title: "Start With Why", author: "Simon Sinek"),
+        Book(title: "Think Like A Monk", author: "Jay Shetty"),
+        Book(title: "Limitless", author: "Jim Kwik")
+    ]
+
+    @State private var randomBook: Book?
+    @State private var selectedBook: Book?
+    @State private var showAlert = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(spacing: 24) {
+                Button {
+                    randomBook = books.randomElement()
+                    showAlert = true
+                } label: {
+                    Label("Show Random Book", systemImage: "books.vertical.fill")
+                        .imageScale(.large)
+                        .controlSize(.large)
+                }
+
+                if let selectedBook {
+                    HStack {
+                        Text("\(selectedBook.title ) by ")
+                        +
+                        Text("\(selectedBook.author )")
+                            .bold()
+                    }
+                } else {
+                    Text("No book is selected yet!")
+                }
+            }
+            .frame(maxHeight: .infinity, alignment: .top)
+            .padding()
+            .toolbar {
+                Button {
+                    print("Add tapped")
+                } label: {
+                    Label("Add book", systemImage: "plus")
+                }
+            }
+            .alert(
+                "Random Book",
+                isPresented: $showAlert,
+                presenting: randomBook
+            ) { book in
+                Button("Done") {
+                    selectedBook = book
+                }
+            } message: { book in
+                Text("You have selected \(book.title) by \(book.author)")
+            }
         }
-        .padding()
     }
 }
 
