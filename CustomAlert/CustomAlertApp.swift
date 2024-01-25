d1ae969dfe224492b0dc8379c83fb805770b075f
Marwa <marwa.niaaj@gmail.com>
2024-01-25 10:01:48 +0400

    Initial Commit

---
+++ CustomAlert/CustomAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAlertApp.swift
+//  CustomAlert
+//
+//  Created by Marwa Abou Niaaj on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
