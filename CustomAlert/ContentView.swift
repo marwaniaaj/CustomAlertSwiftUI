9034d3611d5a23b0724852c61485405a5262e557
Marwa <marwa.niaaj@gmail.com>
2024-01-28 16:29:37 +0400

    Remove old alert.

--- CustomAlert/ContentView.swift
+++ CustomAlert/ContentView.swift
@@ -60,17 +60,6 @@ struct ContentView: View {
                     Label("Show credit", systemImage: "person.bubble")
                 }
             }
-//            .alert(
-//                "Random Book",
-//                isPresented: $showAlert,
-//                presenting: randomBook
-//            ) { book in
-//                Button("Done") {
-//                    selectedBook = book
-//                }
-//            } message: { book in
-//                Text("You have selected \(book.title) by \(book.author)")
-//            }
             .customAlert(
                 "Random Book",
                 isPresented: $showAlert,
